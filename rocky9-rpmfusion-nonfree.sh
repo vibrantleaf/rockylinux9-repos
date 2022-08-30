fbd6bc0bbd073068a18a6f0fa0989b5a487f97dd
julia <bigowo.digital@outlook.com>
2022-08-30 15:15:52 +0800

    Update and rename rocky9-rpmfusion-nonfree to rocky9-rpmfusion-nonfree.sh

--- rocky9-rpmfusion-nonfree.sh
+++ rocky9-rpmfusion-nonfree.sh
@@ -1,7 +1,6 @@
 sudo dnf update -y
 
 # rpmfusion-nonfree
-sudo dnf config-manager --set-enabled crb -y
 sudo dnf install --nogpgcheck -y https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm
 sudo dnf clean all -y
 sudo dnf upgrade --refresh
